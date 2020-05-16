d1ea7a607335a7eab9388eabdec2291479355fbd
JunYeongJeon1 <62225425+JunYeongJeon1@users.noreply.github.com>
2020-05-17 02:16:18 +0900

    ArrayQueue2

---
+++ ArrayQueue2/src/ArrayQueuemain.java
@@ -0,0 +1,23 @@
+public class ArrayQueuemain {
+
+		public static void main(String[] args) {
+
+			ArryQueue<String> queue = new ArryQueue<String>();
+
+			queue.add("apple");  queue.add("orange");
+			queue.add("cherry");  queue.add("pear");  queue.print();
+
+			queue.remove();  queue.print();
+
+			queue.add("grape");  queue.print();
+
+			queue.remove();  queue.print();
+
+			queue.add("lemon");  queue.print();
+			queue.add("mango");  queue.print();
+			queue.add("lime");  queue.print();
+			queue.add("kiwi");  queue.print();
+
+			queue.remove();  queue.print();
+		}
+	}
